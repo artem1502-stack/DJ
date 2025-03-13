6af949304bcc8fc1da7726b2f76b7a58c2bd6638
Ann <Anna.MPM@yandex.ru>
2025-03-13 19:56:07 +0300

    Add base template, registration and forms

--- mymessenger/home/forms.py
+++ mymessenger/home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import Message
 
 
@@ -7,3 +8,19 @@ class MessageForm(forms.Form):
     date_field = forms.DateField(widget=forms.SelectDateWidget)
     time_field = forms.TimeField(widget=forms.TimeInput)
     boolean_field = forms.BooleanField(required=False)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
+    check_password = forms.CharField(widget=forms.PasswordInput, label="Insert password again")
+
+    def clean_check_password(self):
+        c_d = self.cleaned_data
+        if c_d["password"] != c_d["check_password"]:
+            raise forms.ValidationError("Input passwords seem to be different")
+        return c_d["check_password"]
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
--- mymessenger/home/views.py
+++ mymessenger/home/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from .models import Message
-from .forms import MessageForm
+from .forms import MessageForm, UserRegistrationForm
 import sqlite3
 import datetime
 
@@ -19,34 +20,36 @@ def get_messages2():
 
 def save_input_message(request):
     if request.method == "POST":
-        text = request.POST.get("text")
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            text = request.POST.get("text")
 
-        def convert_date():
-            date_field_day = int(request.POST.get("date_field_day"))
-            date_field_month = int(request.POST.get("date_field_month"))
-            date_field_year = int(request.POST.get("date_field_year"))
+            def convert_date():
+                date_field_day = int(request.POST.get("date_field_day"))
+                date_field_month = int(request.POST.get("date_field_month"))
+                date_field_year = int(request.POST.get("date_field_year"))
 
-            time_field = request.POST.get("time_field").split(":")
-            time_hours_field = int(time_field[0])
-            time_minutes_field = int(time_field[1])
-            if len(time_field) == 3:
-                time_seconds_field = int(time_field[2])
-            else:
-                time_seconds_field = 0
+                time_field = request.POST.get("time_field").split(":")
+                time_hours_field = int(time_field[0])
+                time_minutes_field = int(time_field[1])
+                if len(time_field) == 3:
+                    time_seconds_field = int(time_field[2])
+                else:
+                    time_seconds_field = 0
 
-            date_time = datetime.datetime(year=date_field_year,
-                                          month=date_field_month,
-                                          day=date_field_day,
-                                          hour=time_hours_field,
-                                          minute=time_minutes_field,
-                                          second=time_seconds_field)
-            return date_time
+                date_time = datetime.datetime(year=date_field_year,
+                                              month=date_field_month,
+                                              day=date_field_day,
+                                              hour=time_hours_field,
+                                              minute=time_minutes_field,
+                                              second=time_seconds_field)
+                return date_time
 
-        date_time = convert_date()
-        boolean_field = bool(request.POST.get("boolean_field"))
+            date_time = convert_date()
+            boolean_field = bool(request.POST.get("boolean_field"))
 
-        message = Message(content=text, pud_date=date_time, is_read=boolean_field)
-        message.save()
+            message = Message(content=text, pud_date=date_time, is_read=boolean_field)
+            message.save()
 
 
 def index(requests):
@@ -54,3 +57,22 @@ def index(requests):
     messages = get_messages2()
     message_form = MessageForm()
     return render(requests, 'home/index.html', {'messages': messages, 'message_form': message_form})
+
+
+def registration(requests):
+    save_input_message(requests)
+    messages = get_messages2()
+
+    if requests.method == "POST":
+        user_form = UserRegistrationForm(requests.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save()
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+    user_registration_form = UserRegistrationForm()
+    message_form = MessageForm()
+
+    users = User.objects.all()
+    return render(requests, "registration/registration.html", {'user_registration_form': user_registration_form,
+                                                               'users': users})
